fe5f0816c9122f14cd7cb898ff366ae6a6239d14
bgurumur <bgurumur@LIN35006809.corp.capgemini.com>
2022-03-02 12:47:38 +0530

    fristcommint

---
+++ Facebook/src/main/java/com/facebook/Test.java
@@ -0,0 +1,64 @@
+package com.facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium-java_WS\\WorksSpace\\Facebook\\src\\main\\resources\\drivers\\chromedriver.exe");
+		
+		
+		WebDriver driver=new ChromeDriver();
+		
+		//get is a method which is used to open the application url
+		driver.get("https://www.facebook.com/");
+		//id ,name,Text,LinkText,Xpath,cssSelector TagName, class
+		
+		
+		//id
+		
+		
+
+	
+
+		
+		
+		
+		
+	
+		
+		///
+		/*
+		 * Person raghu=driver.findElement(By.posportnumber("sdasdasdfasf")); Person
+		 * ashwin=driver.findElement(By.voterid("!323123123"));
+		 * 
+		 * Person naresh=Addhar("31234234324")
+		 */
+		
+		
+	//close(); methodnwhich is use to close the current browser
+		
+		//driver.close();
+		
+		
+		//
+		//quit() -is a method ,it will close all the tabs or browsers
+		//driver.quit();
+		
+		
+		///
+		
+		/*
+		 * id empolyee id- name name class dob Text LinkText PartialLinktext cssSelector
+		 * xpath TagName
+		 */
+	
+	}
+
+}
